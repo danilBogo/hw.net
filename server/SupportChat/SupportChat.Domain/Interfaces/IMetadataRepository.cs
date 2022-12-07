3c5817dfe18483d7302ac2187a99c8c424f1ffbf
danilBogo <danil.bogo@gmail.com>
2022-12-08 01:46:24 +0300

    tiny little changes

---
+++ server/SupportChat/SupportChat.Domain/Interfaces/IMetadataRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupportChat.Domain.Models.Files;
+
+namespace SupportChat.Domain.Interfaces;
+
+public interface IMetadataRepository
+{
+    Task<Metadata> GetMetadataByFileIdAsync(string messageId);
+
+    Task<Metadata> CreateMetadataAsync(Metadata metadata);
+}
---
+++ server/SupportChat/SupportChat.Domain/Repositories/MetadataRepository.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using SupportChat.Domain.Interfaces;
+using SupportChat.Domain.Models.Files;
+
+namespace SupportChat.Domain.Repositories;
+
+public class MetadataRepository : IMetadataRepository
+{
+    private IMongoCollection<Metadata> Metadata => _database.GetCollection<Metadata>(nameof(Models.Files.Metadata));
+    private readonly IMongoDatabase _database;
+
+    public MetadataRepository(IMongoDatabase database)
+    {
+        _database = database;
+    }
+
+    // public MetadataRepository(IMongoDbConfiguration configuration)
+    // {
+    //     _database = new MongoClient(configuration.ConnectionString).GetDatabase(configuration.Database);
+    // }
+
+    public async Task<Metadata> GetMetadataByFileIdAsync(string fileId) =>
+        await Metadata.Find(f => f.FileId == fileId).FirstOrDefaultAsync();
+
+
+    public async Task<Metadata> CreateMetadataAsync(Metadata metadata)
+    {
+        await Metadata.InsertOneAsync(metadata);
+        return metadata;
+    }
+}
